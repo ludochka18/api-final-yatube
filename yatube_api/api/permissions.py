36869a355d39e4fc3407fd1f6fb1d7c5af08a465
Ludmila Barsukova <ludmilabarsukova13@gmail.com>
2026-06-05 10:43:40 +0000

    1

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -2,17 +2,14 @@ from rest_framework import permissions
 
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
+
     def has_permission(self, request, view):
         return (
             request.method in permissions.SAFE_METHODS
             or request.user.is_authenticated
         )
 
-    def has_object_permissoin(self, request, view, obj):
+    def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.author == request.user
-
-# Неавторизованные пользователи могут только читать данные,
-# авторизованные — создавать контент, а изменять
-# и удалять объект может только его автор
