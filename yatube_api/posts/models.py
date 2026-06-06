05aa207ad135980e69df513ea73eb971f5665a1d
Ludmila Barsukova <ludmilabarsukova13@gmail.com>
2026-06-06 10:50:07 +0000

    1

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -1,36 +1,52 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
+from posts.constants import TEXT_LENGTH
+
 User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
-    description = models.TextField(blank=True)
+    title = models.CharField(
+        'Название',
+        max_length=200
+    )
+    slug = models.SlugField(
+        'Слаг',
+        unique=True
+    )
+    description = models.TextField(
+        'Описание',
+        blank=True
+    )
 
     def __str__(self):
         return self.title
 
 
 class Post(models.Model):
-    text = models.TextField()
+    text = models.TextField(
+        'Текст публикации'
+    )
     pub_date = models.DateTimeField(
         'Дата публикации',
         auto_now_add=True
     )
     author = models.ForeignKey(
         User,
+        verbose_name='Автор',
         on_delete=models.CASCADE,
         related_name='posts'
     )
     image = models.ImageField(
+        'Изображение',
         upload_to='posts/',
         null=True,
         blank=True
     )
     group = models.ForeignKey(
         Group,
+        verbose_name='Группа',
         on_delete=models.SET_NULL,
         related_name='posts',
         blank=True,
@@ -38,24 +54,28 @@ class Post(models.Model):
     )
 
     class Meta:
-        ordering = ('id',)
+        ordering = ('-pub_date',)
 
     def __str__(self):
-        return self.text[:15]
+        return self.text[:TEXT_LENGTH]
 
 
 class Comment(models.Model):
     author = models.ForeignKey(
         User,
+        verbose_name='Автор',
         on_delete=models.CASCADE,
         related_name='comments'
     )
     post = models.ForeignKey(
         Post,
+        verbose_name='Публикация',
         on_delete=models.CASCADE,
         related_name='comments'
     )
-    text = models.TextField()
+    text = models.TextField(
+        'Текст комментария'
+    )
     created = models.DateTimeField(
         'Дата добавления',
         auto_now_add=True,
@@ -63,20 +83,22 @@ class Comment(models.Model):
     )
 
     class Meta:
-        ordering = ('id',)
+        ordering = ('created',)
 
     def __str__(self):
-        return self.text[:15]
+        return self.text[:TEXT_LENGTH]
 
 
 class Follow(models.Model):
     user = models.ForeignKey(
         User,
+        verbose_name='Подписчик',
         on_delete=models.CASCADE,
         related_name='follower'
     )
     following = models.ForeignKey(
         User,
+        verbose_name='Автор',
         on_delete=models.CASCADE,
         related_name='following'
     )
